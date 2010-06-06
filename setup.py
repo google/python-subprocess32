75718c3700f06f45a7ac2a8d8e3cdd8d790e2b4b
Gregory P. Smith <greg@krypto.org>
2010-06-06 01:07:31 -0700

    comment updates.

--- setup.py
+++ setup.py
@@ -13,11 +13,12 @@ def main():
     setup(
       name='subprocess32',
       version='3.2.0',
-      description='A backport of the subprocess module from Python 3.2.',
+      description='Backport of the subprocess module from Python 3.2 for use on 2.x.',
       long_description="""
 This is a backport of the subprocess standard library module from
-Python 3.2.  It includes many bugfixes and features, on POSIX systems
-it should be much more reliable when used in threaded applications.""",
+Python 3.2 for use on Python 2.x.
+It includes bugfixes and new features.  On POSIX systems it should
+be much more reliable when used in threaded applications.""",
       license='PSF license',
 
       maintainer='Gregory P. Smith',
