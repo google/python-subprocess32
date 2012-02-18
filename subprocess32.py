1bbe2231ee01049985d1af38e7963e8ba28bc206
Gregory P. Smith <greg@krypto.org>
2012-02-17 21:31:51 -0800

    make docstring match what the next patch expects.

--- subprocess32.py
+++ subprocess32.py
@@ -32,7 +32,7 @@ This module defines one class called Popen:
 
 class Popen(args, bufsize=0, executable=None,
             stdin=None, stdout=None, stderr=None,
-            preexec_fn=None, close_fds=True, shell=False,
+            preexec_fn=None, close_fds=_PLATFORM_DEFAULT, shell=False,
             cwd=None, env=None, universal_newlines=False,
             startupinfo=None, creationflags=0,
             restore_signals=True, start_new_session=False):
