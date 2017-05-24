e0817ab6f30f48162893f461fed47487f1cbac6a
Gregory P. Smith [Google] <greg@krypto.org>
2017-05-24 06:52:44 +0000

    Add a ChangeLog entry for run() raise the version.
    Also updates some of the package description text now that we pull
    in a feature from 3.5 as well as 3.2 and 3.3.

--- setup.py
+++ setup.py
@@ -24,16 +24,16 @@ def main():
 
     setup(
       name='subprocess32',
-      version='3.2.8.dev',
-      description='A backport of the subprocess module from Python 3.2/3.3 for use on 2.x.',
+      version='3.5.0',
+      description='A backport of the subprocess module from Python 3 for use on 2.x.',
       long_description="""\
 This is a backport of the subprocess standard library module from
-Python 3.2 & 3.3 for use on Python 2.
+Python 3.2 - 3.5 for use on Python 2.
 
 It includes bugfixes and some new features.  On POSIX systems it is
 guaranteed to be reliable when used in threaded applications.
-It includes timeout support from Python 3.3 but otherwise matches
-3.2's API.
+It includes timeout support from Python 3.3 and the run() API from 3.5
+but otherwise matches 3.2's API.
 
 It has not been tested by the author on Windows.
 
